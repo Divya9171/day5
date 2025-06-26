3e7f917a6034359a120360b0552a225ba754d1c7
Divya9171 <divyamagdum9@gmail.com>
2025-06-26 13:04:08 +0530

    Add files via upload

---
+++ manager.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class manager {
+	double bonus;
+
+	public Manager() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Manager(int empid, double salary, double bonus) {
+		super(empid, salary);
+		this.bonus = bonus;
+	}
+
+	@Override
+	public void accept(Scanner sc) {
+		super.accept(sc);
+		System.out.print("Enter bonus - ");
+		bonus = sc.nextDouble();
+	}
+
+	@Override
+	public void display() {
+		super.display();
+		System.out.println("Bonus - " + bonus);
+	}
+
+}
---
+++ program1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class program1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			Manager m = new Manager();
+			m.accept(sc);
+			m.display();
+
+
+	}
+
+}
